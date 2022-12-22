3715596d54583ca23dceacbc712441ed9d12c78e
Rohitranpise <rohitranpise2758@gmail.com>
2022-12-22 17:06:03 +0530

    first commit

---
+++ ecommerce/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+        <p>this is my app</p>
+        <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
