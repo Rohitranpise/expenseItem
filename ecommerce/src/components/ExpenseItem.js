42294540fd2c60b673d515efecd1ceb4ef92dade
Rohitranpise <rohitranpise2758@gmail.com>
2022-12-22 18:27:00 +0530

    second commit

--- ecommerce/src/components/ExpenseItem.js
+++ ecommerce/src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
-  return <h2>this is ExpenseItem!</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 20 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$290</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
